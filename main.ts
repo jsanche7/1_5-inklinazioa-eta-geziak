dcbe00cc979a5712b91c973b297f78d4aecab063
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2021-10-27 14:02:20 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+function biratu_eskuina () {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.NorthWest)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.NorthEast)
+}
+function biratu_ezkerra () {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.NorthWest)
+}
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        biratu_ezkerra()
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        biratu_eskuina()
+    }
+})
